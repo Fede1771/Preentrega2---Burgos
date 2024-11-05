ee9a988701c243d123f6525a43951d540d95d2f2
Fede1771 <federicoburgos98@gmail.com>
2024-11-04 21:33:44 -0300

    se arregla navbar y cartwidget

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import CartWidget from './CartWidget';
 
 const Navbar = () => {
-  // Eliminar "Calzado" de las categorías
   const categorias = ["Ropa", "Accesorios", "Electrónica"];
 
   return (
@@ -14,11 +14,11 @@ const Navbar = () => {
 
       <ul className="categories">
         <li>
-          <a href="/">Inicio</a>
+          <Link to="/">Inicio</Link>
         </li>
         {categorias.map((categoria, index) => (
           <li key={index}>
-            <a href={`/productos/${categoria.toLowerCase()}`}>{categoria}</a>
+            <Link to={`/productos/${categoria.toLowerCase()}`}>{categoria}</Link>
           </li>
         ))}
       </ul>
